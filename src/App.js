864079541aebbdf35dcf946d901d005b4083426e
jonathashyperlocal <jonathas.rivero@hyperlocal.com.br>
2024-10-30 08:47:39 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-7xl sm:mx-auto">
         <div className="relative px-4 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <h1>Dashboard Avec Pro</h1> {/* Teste básico */}
           <Dashboard />
         </div>
       </div>
